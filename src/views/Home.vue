cef819732c10a230822cbec1e8b0fbbb32bb5df1
fyz20010609 <975911091@qq.com>
2022-02-06 18:21:17 +0800

    构建项目基本结构

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-button type="primary" :icon="Edit"></el-button>
+    <el-button type="primary" :icon="Share"></el-button>
+    <el-button type="primary" :icon="Delete"></el-button>
+    <el-button type="primary" :icon="Search">Search</el-button>
+    <el-button type="primary">
+      Upload<el-icon class="el-icon--right"><Upload /></el-icon>
+    </el-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
